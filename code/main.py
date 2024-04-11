ef4f1ed5e0a9deb0b9e9d81ad591a8fc87c9e7e1
Zantoffel <leonk@HYBLDL-G1329FCR>
2024-04-11 22:25:23 +0200

    add next_tile + refresh to button interupt

--- code/main.py
+++ code/main.py
@@ -5,11 +5,25 @@ from picozero import Button
 def funcCall():
     print("pressed")
     
+def button_next():
+    dynTile.next_tile()
+    display.display()
+
+def button_prev():
+    dynTile.prev_tile()
+    display.display()
+   
 if __name__=='__main__':
     
-    button = Button(18)
+    button = Button(4)
     button.when_pressed= funcCall
-    
+    """
+    button_next = Button(5) #KEY1 GP3 pin 5
+    button_prev = Button(4)  #KEY0 GP2 pin 4
+    button_next.when_pressed= button_next
+    button_prev.when_pressed= button_prev
+    """
+
     dynTile = tiles.dynamic_Tile(300, 300, [
             tiles.Template_Tile(0,0),
             tiles.Calender_Tile(0,0),
