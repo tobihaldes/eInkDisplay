46407098133ef27aa706abffa607c6483ddd337a
Tobias Haldenwanger <tobias.haldenwanger@foerch.de>
2024-04-16 13:01:28 +0200

    Add news.py (working) and edited calendar.py (working)

--- code/apis/calendar.py
+++ code/apis/calendar.py
@@ -1,52 +1,47 @@
-import ussl
-import usocket
+import urequests
 
-def get_https_content(url):
-    _, _, host, path = url.split('/', 3)
-    addr_info = usocket.getaddrinfo(host, 443)
-    addr = addr_info[0][-1]
+# URL anpassen von webcal zu http
+url = 'http://p139-caldav.icloud.com/published/2/MTE1NzQwNjE0NzQxMTU3NDmLjpu4-S1Y9s3ZY6FOrrHnIwf0-kAOhn-6sr24tcTFaqodbcvwQ-1iUIyMWm_Q-QI6qieG29wXJSUU0hdN6JI'
 
-    s = usocket.socket()
-    s.connect(addr)
-    s = ussl.wrap_socket(s, server_hostname=host)
+# Funktion zum Extrahieren der Ereignisse aus den iCalendar-Daten
+def format_datetime(datetime_str):
+    if 'T' in datetime_str:
+        date_part, time_part = datetime_str.split('T')
+        formatted_date = f"{date_part[:4]}-{date_part[4:6]}-{date_part[6:]}"
+        formatted_time = f"{time_part[:2]}:{time_part[2:4]}"
+        return f"{formatted_date} {formatted_time}"
+    return f"{datetime_str[:4]}-{datetime_str[4:6]}-{datetime_str[6:]}"
 
-    s.write(b"GET /{} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(path, host))
-
-    in_content = False
-    content = ''
-    while True:
-        data = s.read(512)
-        if data:
-            # Beginn des Inhalts nach dem Header-Ende suchen
-            if not in_content:
-                data_str = data.decode('utf-8')
-                if '\r\n\r\n' in data_str:
-                    headers, content = data_str.split('\r\n\r\n', 1)
-                    in_content = True
-            else:
-                content += data.decode('utf-8')
-        else:
-            break
-
-    s.close()
-    return content
-
-def parse_ics(data):
+# Funktion zum Extrahieren der Ereignisse aus den iCalendar-Daten
+def extract_events(ical_data):
+    events = []
+    lines = ical_data.splitlines()
     in_event = False
-    event_count = 0
-    for line in data.splitlines():
-        if line.startswith("BEGIN:VEVENT"):
+    for line in lines:
+        if line.startswith('BEGIN:VEVENT'):
             in_event = True
-        elif line.startswith("END:VEVENT"):
+            current_event = {}
+        elif line.startswith('END:VEVENT'):
+            events.append(current_event)
             in_event = False
-            event_count += 1
-            if event_count >= 5:
-                break
         elif in_event:
-            if line.startswith("SUMMARY:"):
-                print("Summary:", line[len("SUMMARY:"):].strip())
-            elif line.startswith("DTSTART;") or line.startswith("DTSTART:"):
-                print("Start Time:", line.split(':', 1)[1].strip())
+            if line.startswith('SUMMARY:'):
+                current_event['summary'] = line.split(':', 1)[1]
+            elif 'DTSTART' in line:
+                time_part = line.split(':', 1)[1]
+                current_event['start_time'] = format_datetime(time_part)
 
-ics_content = get_https_content("https://calendar.google.com/calendar/ical/de.german%23holiday%40group.v.calendar.google.com/public/basic.ics")
-parse_ics(ics_content)
+    return events[:5]
+
+# HTTP-Anfrage senden, um die iCalendar-Daten zu erhalten
+response = urequests.get(url)
+
+if response.status_code == 200:
+    # Ereignisse extrahieren und die nächsten fünf ausgeben
+    next_events = extract_events(response.text)
+    for event in next_events:
+        print(f"Event: {event['summary']}, Start Time: {event['start_time']}")
+else:
+    print('Fehler beim Abrufen der Daten:', response.status_code)
+
+response.close()
