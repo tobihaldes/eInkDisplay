eafeb11c066dc90fafce9769c775c172388a1445
zantoffel <leon.karais200@gmail.com>
2024-05-16 14:31:19 +0200

    Cleaned up code, added refresh button

--- code/main.py
+++ code/main.py
@@ -5,8 +5,30 @@ import gc
 import micropython
 from picozero import Button
 
+############################################################################
+###          CUSTOMIZE LAYOUT HERE, TILES AT POSITION (X, Y)             ###
+############################################################################
+
+# Cycle throgh these tiles in the gallery using the next and previous Button
+gallery = tiles.tile_gallery([
+            #tiles.Weather_Tile_l(0,0),
+            #tiles.ToDo_Tile(0,0),
+            tiles.Calendar_Tile(0,0),
+            tiles.News_Tile(0,0),
+            ])
+
+# Add static tiles that will be shown contiously
+layout = [
+            tiles.Clock_Tile_s(560, 0),
+            tiles.Weather_Tile_s(560, 240),
+            gallery,
+         ]
+
+############################################################################
+
 update_flag = 0
 
+# Button functions
 def button_next():
     gallery.next_tile()
     global update_flag
@@ -18,41 +40,29 @@ def button_prev():
     global update_flag
     update_flag = 0
     print("previous tile")
-    
+
+def button_ref():
+    global update_flag
+    update_flag = 0
+    print("refresh screen")
+
+# Main
 if __name__=='__main__':
     gc.enable()
     gc.collect()
     
-    # Initialisierung Taster
-    """
-    btn_next = Pin(3, Pin.IN, Pin.PULL_UP)  
-    btn_next.irq(trigger=Pin.IRQ_RISING, handler=button_next)
-    btn_prev = Pin(2, Pin.IN, Pin.PULL_UP)    
-    btn_prev.irq(trigger=Pin.IRQ_RISING, handler=button_prev)
-    """
-    btn_next = Button(3)                  #KEY1 GP3 pin 5
+    # Init buttons
+    btn_next = Button(3)                  # KEY1 GP3 pin 5
     btn_next.when_pressed = button_next
-    btn_prev = Button(2)                  #KEY0 GP2 pin 4
+    btn_prev = Button(2)                  # KEY0 GP2 pin 4
     btn_prev.when_pressed = button_prev
+    #btn_ref = Button(4)                  # GP4 pin 6
+    #btn_next.when_pressed = button_ref
     
-    #Layout
-    gallery = tiles.tile_gallery([
-            #tiles.Weather_Tile_l(0,0),
-            #tiles.ToDo_Tile(0,0),
-            tiles.Calendar_Tile(0,0),
-            tiles.News_Tile(0,0),
-            ])
-    
-    layout = [
-        tiles.Clock_Tile_s(560, 0),
-        tiles.Weather_Tile_s(560, 240),
-        gallery,
-        ]
-    
-    # initiate Display Objekt
+    # Initiate display object
     display = driver.EPD_7in5_B(layout)
-    #micropython.mem_info(1)
     
+    # Application Loop to refresh screen
     while True:
         update_flag = 60
         display.init()
@@ -60,7 +70,7 @@ if __name__=='__main__':
         display.delay_ms(1000)
         display.sleep()
         display.delay_ms(1000)
-        #sleep for 1s, check if button was pressed
+        # Sleep for 1s, check if button was pressed
         while update_flag > 0:
             update_flag -= 1
             #machine.lightsleep(1000)  #use for Powersaving in Production
